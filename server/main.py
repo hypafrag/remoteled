1852b8b13a29ad7c5842c019c4fd85bea8b30642
escobar <escobar@mosk.ru>
2021-09-21 21:49:07 +0700

    detecting arduino

--- server/main.py
+++ server/main.py
@@ -1,7 +1,12 @@
 import serial
+import serial.tools.list_ports
 import struct
 import random
 import time
+import sys
+
+ARDUINO_VID = 0x2341
+ARDUINO_LEONARDO_PID = 0x8036
 
 
 def open_serial(name):
@@ -14,19 +19,27 @@ def write_pic(port, pic):
     data = b''
     for px in pic:
         data += struct.pack('BBB', *px)
+    print('write started')
     port.write(data)
+    print('write finished')
 
 
-def main():
-    with open_serial('COM12') as port:
+def main(port_name):
+    with open_serial(port_name) as port:
         while True:
             pic = []
             for i in range(300):
                 pic.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             write_pic(port, pic)
-            time.sleep(0.4)
+            time.sleep(1)
     return 0
 
 
 if __name__ == '__main__':
-    exit(main())
+    detected_ports = [p.device
+                      for p in serial.tools.list_ports.comports()
+                      if p.vid == ARDUINO_VID and p.pid == ARDUINO_LEONARDO_PID]
+
+    print('UART:', *detected_ports, file=sys.stderr)
+
+    exit(main(detected_ports[0]))
